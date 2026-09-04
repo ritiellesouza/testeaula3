e64ec5a54ca4c5967e3ed24a5976fef3713173c0
Ritielle Souza <ritiellesouza@gmail.com>
2026-09-04 20:13:42 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("=== CALCULADORA DE MÉDIA ===")
+
+nome = input("Nome do aluno: ")
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+print("Aluno:", nome)
+print("Média:", media)
